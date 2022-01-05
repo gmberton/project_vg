f54d824b7cd84f6dbbf6659b63849e639aac91a2
hallbicocca <49003913+hallbicocca@users.noreply.github.com>
2022-01-05 10:52:08 +0100

    Add Netvlad implementation

--- parser.py
+++ parser.py
@@ -6,6 +6,10 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description="Benchmarking Visual Geolocalization",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     # Training parameters
+
+    parser.add_argument("--use_netvlad",
+                        help="Specify if NetVlad must be used")
+
     parser.add_argument("--train_batch_size", type=int, default=4,
                         help="Number of triplets (query, pos, negs) in a batch. Each triplet consists of 12 images")
     parser.add_argument("--infer_batch_size", type=int, default=16,
@@ -15,7 +19,8 @@ def parse_arguments():
     parser.add_argument("--epochs_num", type=int, default=50,
                         help="Maximum number of epochs to train for")
     parser.add_argument("--patience", type=int, default=3)
-    parser.add_argument("--lr", type=float, default=0.00001, help="Learning rate")
+    parser.add_argument("--lr", type=float,
+                        default=0.00001, help="Learning rate")
     parser.add_argument("--cache_refresh_rate", type=int, default=1000,
                         help="How often to refresh cache, in number of queries")
     parser.add_argument("--queries_per_epoch", type=int, default=5000,
@@ -26,21 +31,25 @@ def parse_arguments():
                         help="How many negatives to use to compute the hardest ones")
     # Other parameters
     parser.add_argument("--seed", type=int, default=0)
-    parser.add_argument("--device", type=str, default="cuda", choices=["cuda", "cpu"])
-    parser.add_argument("--num_workers", type=int, default=8, help="num_workers for all dataloaders")
-    parser.add_argument("--val_positive_dist_threshold", type=int, default=25, help="Val/test threshold in meters")
-    parser.add_argument("--train_positives_dist_threshold", type=int, default=10, help="Train threshold in meters")
+    parser.add_argument("--device", type=str,
+                        default="cuda", choices=["cuda", "cpu"])
+    parser.add_argument("--num_workers", type=int, default=8,
+                        help="num_workers for all dataloaders")
+    parser.add_argument("--val_positive_dist_threshold", type=int,
+                        default=25, help="Val/test threshold in meters")
+    parser.add_argument("--train_positives_dist_threshold",
+                        type=int, default=10, help="Train threshold in meters")
     parser.add_argument('--recall_values', type=int, default=[1, 5, 10, 20], nargs="+",
                         help="Recalls to be computed, such as R@5.")
     # Paths parameters
-    parser.add_argument("--datasets_folder", type=str, required=True, help="Path with datasets")
+    parser.add_argument("--datasets_folder", type=str,
+                        required=True, help="Path with datasets")
     parser.add_argument("--exp_name", type=str, default="default",
                         help="Folder name of the current run (saved in ./runs/)")
-    
+
     args = parser.parse_args()
-    
+
     if args.queries_per_epoch % args.cache_refresh_rate != 0:
         raise ValueError("Ensure that queries_per_epoch is divisible by cache_refresh_rate, " +
                          f"because {args.queries_per_epoch} is not divisible by {args.cache_refresh_rate}")
     return args
-
