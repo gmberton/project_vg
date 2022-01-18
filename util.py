76e5ce2f009c42f8178bcf247c47c88b87f1449d
MarcoBramini <braminim@gmail.com>
2022-01-18 17:56:37 +0100

    Make image and make sizes parametric

--- util.py
+++ util.py
@@ -13,21 +13,31 @@ def save_checkpoint(args, state, is_best, filename):
         shutil.copyfile(model_path, join(args.output_folder, "best_model.pth"))
 
 
+def inverse_normalize(tensor, mean, std):
+    for t, m, s in zip(tensor, mean, std):
+        t.mul_(s).add_(m)
+    return tensor
+
+
 def save_attention_mask(images, masks):
     W = images[0].shape[2]
     H = images[0].shape[1]
+
+    mask_W = W//16
+    mask_H = H//16
 
     # Create figure and array of axes
     fig, ax = plt.subplots(nrows=4, ncols=4, figsize=[10, 10])
 
     for i, axi in enumerate(ax.flat):
-        image = images[i].permute(1, 2, 0)
-        mask = masks[i].reshape(30, 40)
+        image = inverse_normalize(tensor=images[i], mean=(
+            0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)).permute(1, 2, 0)
+        mask = masks[i].reshape(mask_H, mask_W)
 
         filtered_mask = gaussian_filter(mask, 3)
 
-        axi.imshow(image[:, :, 0], cmap='gray')
-        axi.imshow(filtered_mask[:, :], cmap='rainbow', interpolation='bilinear',
-                   alpha=0.8, extent=(0, 640, 480, 0))
+        axi.imshow(image[:, :, :])
+        axi.imshow(filtered_mask[:, :], cmap='turbo', interpolation='bilinear',
+                   alpha=0.6, extent=(0, W, H, 0))
 
     plt.show()
